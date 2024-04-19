e93200c415a0ef990c67395a742f52b51f05c8c1
legregz <132829354+legregz@users.noreply.github.com>
2024-04-19 22:12:21 +0200

    dockstring

--- stormkey/main.py
+++ stormkey/main.py
@@ -8,24 +8,67 @@ class Stormkey:
         self.headers = {"X-API-Key": self.serviceKey, "Content-Type": "application/json"}
 
     def create(self, name:str, ownerId:str):
+        """Create a new API key.
+
+        Args:
+            name (str) -- the name of the API key
+            ownerId (str) -- key owner Id
+
+        Returns:
+            key (dict) -- Key object
+        """
+
         data = json.dumps({"name": name, "ownerId": ownerId})
 
         response = requests.post(f"{self.host}/api/v1/apps/{self.appId}/keys", data = data, headers = self.headers)
         return response.json()["key"]
 
     def update(self, key_id:str, name:str, ownerId: str):
+        """Update the API key.
+
+        Args:
+            key_id (str) -- the id of the key
+            name (str) -- the name of the API key
+            ownerId (str) -- key owner Id
+
+        Returns:
+            key (dict) -- Key object
+        """
+
         data = json.dumps({"name": name, "ownerId": ownerId})
 
         response = requests.patch(f"{self.host}/api/v1/apps/{self.appId}/keys/{key_id}", data = data, headers = self.headers)
         return response.json()["key"]
 
     def delete(self, key_id:str, name:str):
+        """Delete the API key.
+        
+        Args:
+            key_id (str) -- the id of the key
+            name (str) -- the name of the API key
+
+        Returns:
+            key (dict) -- Key object
+        """
+
         data = json.dumps({"name": name})
 
         response = requests.delete(f"{self.host}/api/v1/apps/{self.appId}/keys/{key_id}", data = data, headers = self.headers)
         return response.json()["key"]
     
     def verify(self, key:str):
+        """Verify the Api key.
+
+        Args:
+            key (str) -- your secret API key
+
+        Returns:
+            {
+                "valid": valid (bool) -- the validity of the key
+                "expired": expired (bool) -- True if the key is expired
+                "key": key (dict) -- Key object
+            }
+        """
         data = json.dumps({"key": key})
 
         response = requests.post(f"{self.host}/api/v1/apps/{self.appId}/keys/verify", data = data, headers = self.headers)
