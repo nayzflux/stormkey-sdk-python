c733e924021093a9b03e963b0a94021fe8b7ee4b
nayzflux <55506519+nayzflux@users.noreply.github.com>
2024-04-19 21:41:38 +0200

    fix: fix update and delete

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
   name="stormkey",
-  version="0.1.1",
+  version="0.1.2",
   packages=find_packages(),
   install_requires=[],
   long_description=description,
--- stormkey/main.py
+++ stormkey/main.py
@@ -13,10 +13,16 @@ class Stormkey:
         response = requests.post(f"{self.host}/api/v1/apps/{self.appId}/keys", data = data, headers = self.headers)
         return response.json()["key"]
 
-    def update(self, name:str, ownerId:str):
+    def update(self, key_id:str, name:str, ownerId: str):
         data = json.dumps({"name": name, "ownerId": ownerId})
 
-        response = requests.patch(f"{self.host}/api/v1/apps/{self.appId}/keys", data = data, headers = self.headers)
+        response = requests.patch(f"{self.host}/api/v1/apps/{self.appId}/keys/{key_id}", data = data, headers = self.headers)
+        return response.json()["key"]
+
+    def delete(self, key_id:str, name:str):
+        data = json.dumps({"name": name})
+
+        response = requests.delete(f"{self.host}/api/v1/apps/{self.appId}/keys/{key_id}", data = data, headers = self.headers)
         return response.json()["key"]
     
     def verify(self, key:str):
@@ -24,9 +30,3 @@ class Stormkey:
 
         response = requests.post(f"{self.host}/api/v1/apps/{self.appId}/keys/verify", data = data, headers = self.headers)
         return response.json()
-
-    def delete(self, name:str):
-        data = json.dumps({"name": name})
-
-        response = requests.delete(f"{self.host}/api/v1/apps/{self.appId}/keys", data = data, headers = self.headers)
-        return response.json()["key"]
