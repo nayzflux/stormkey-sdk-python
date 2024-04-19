11506d6e1208fb0395b0a3b25e3e36e221744c4a
legregz <132829354+legregz@users.noreply.github.com>
2024-04-19 22:52:11 +0200

    fix: missing version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
   name="stormkey",
-  version="0.1.3",
+  version="0.1.4",
   packages=find_packages(),
   install_requires=[],
   long_description=description,
